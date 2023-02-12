20334159603d800c8bcff0825ebc91cb278cac33
pvshafeeq <61697146+pvshafeeq@users.noreply.github.com>
2023-02-12 21:29:46 +0300

    Update Employee.js

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const EmployeeSchema = new mongoose.Schema({
     name: String,
-    position: String,
+    designation: String,
     location: String,
     salary: String
 });
